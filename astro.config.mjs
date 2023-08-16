3808e558fef6fff40a1ed8e4a6af55ef13117e59
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-08-16 15:23:32 +1200

    Packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
@@ -9,7 +10,54 @@ import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown(), react(), vue(), compressor()],
+  integrations: [storyblok({
+    //accessToken: env.STORYBLOK_TOKEN,
+    accessToken: 'p3nWauZqXhuE8lNTLndLHgtt',
+    apiOptions: {
+      region: 'us'
+    },
+    components: {
+      page: 'storyblok/contentTypes/Page',
+      siteSection: 'storyblok/contentTypes/SiteSection',
+      contentSection: 'storyblok/layout/ContentSection',
+      copyBlock: 'storyblok/widgets/CopyBlock',
+      hero: 'storyblok/widgets/Hero',
+      htmlBlock: 'storyblok/widgets/HtmlBlock',
+      linkImageButton: 'storyblok/widgets/HtmlBlock',
+      menuItem: 'storyblok/widgets/MenuItem',
+      titleBlock: 'storyblok/widgets/TitleBlock',
+      youtubeVideo: 'storyblok/widgets/YoutubeVideo'
+    }
+  }),
+  tailwind(), 
+  partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+      logCalls: true,
+      logGetters: true,
+      logSetters: true,
+      logImageRequests: true,
+      logMainAccess: true,
+      logSendBeaconRequests: true,
+      logStackTraces: false,
+      logScriptExecution: true
+    }
+  }), 
+  react(), 
+  vue(), 
+  compressor()],
   output: "server",
+  vite: {
+    server: {
+      host: "localhost",
+      port: 3000,
+      cors: true,
+      headers: {
+        "X-Frame-Options": "ALLOW-FROM https://app.storyblok.com",
+        "Content-Security-Policy": "frame-ancestors https://app.storyblok.com;"
+      }
+    }
+  },
   adapter: netlify()
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['"Gotham Narrow"', 'sans-serif']
 	},
-	plugins: [],
+		extend: {
+
+			typography: {
+
+				DEFAULT: {
+					// Custom CSS here ?
+					css: {
+						img:{
+							width: '100%',
+						},
+						p: {
+							fontStyle: 'normal',
+							fontWeight: 400,
+							fontLeading: 'normal',
+							lineHeight: '1.5em',
+						},
+						hr: {
+							borderColor: 'var(--tw-prose-text)',
+							borderTopWidth: 1,
+						},
+						'blockquote p:first-of-type::before': {
+							content: '""',
+						  },
+						  'blockquote p:last-of-type::after': {
+							content: '""',
+}
+					},
+				},
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/container-queries'),
+	],
+	darkMode: 'class',
+
 }
