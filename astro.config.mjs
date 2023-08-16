a0adbd09554e1eede9d7b75e1cfe798b6571b38b
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-08-16 15:07:54 +1200

    packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify/functions";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), partytown(), react(), vue(), compressor()],
+  output: "server",
+  adapter: netlify()
+});
