aa5ddf7e6a93b02051e64082284b95eb77bed412
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-08-16 14:59:21 +1200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,4 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import tailwind from '@astrojs/tailwind'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  integrations: [
-    storyblok({
-      //accessToken: env.STORYBLOK_TOKEN,
-      accessToken: 'p3nWauZqXhuE8lNTLndLHgtt',
-      apiOptions: {
-        region: 'us',
-      },
-      components: {
-        page: 'storyblok/contentTypes/Page',
-        siteSection: 'storyblok/contentTypes/SiteSection',        
-        contentSection: 'storyblok/layout/ContentSection',
-        copyBlock: 'storyblok/widgets/CopyBlock',
-        hero: 'storyblok/widgets/Hero',
-        htmlBlock: 'storyblok/widgets/HtmlBlock',
-        linkImageButton: 'storyblok/widgets/HtmlBlock',
-        menuItem: 'storyblok/widgets/MenuItem',
-        titleBlock: 'storyblok/widgets/TitleBlock',
-        youtubeVideo: 'storyblok/widgets/YoutubeVideo',
-      },
-    }),
-    tailwind(),
-  ],
-  vite: {
-  },
-})
+export default defineConfig({});
